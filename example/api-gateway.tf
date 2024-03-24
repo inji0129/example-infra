4373a893e93bcd3b11cc902615f8dc89dd8b5d6c
kwangbok <kblee@healthport.co.kr>
2024-03-25 00:49:36 +0900

    feat: api-gateway

--- example/api-gateway.tf
+++ example/api-gateway.tf
@@ -9,7 +9,7 @@ resource "aws_apigatewayv2_integration" "this" {
   payload_format_version = "2.0"
 }
 
-resource "aws_apigatewayv2_route" "this" {
+resource "aws_apigatewayv2_route" "default" {
   api_id    = aws_apigatewayv2_api.this.id
   route_key = "$default"
 }
