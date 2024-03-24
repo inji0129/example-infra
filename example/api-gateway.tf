a6f9d0053b711793a135283bea82cf90290f4aa6
kwangbok <kblee@healthport.co.kr>
2024-03-25 00:44:21 +0900

    feat: api-gateway

---
+++ example/api-gateway.tf
@@ -0,0 +1,14 @@
+resource "aws_apigatewayv2_api" "this" {
+  name          = "${var.project_name}-${var.stage}-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "this" {
+  api_id           = aws_apigatewayv2_api.this.id
+  integration_type = "HTTP_PROXY"
+}
+
+resource "aws_apigatewayv2_route" "this" {
+  api_id    = aws_apigatewayv2_api.this.id
+  route_key = "$default"
+}
--- example/variable.tf
+++ example/variable.tf
@@ -1,3 +1,13 @@
+variable "project_name" {
+  type        = string
+  description = "project name"
+}
+
+variable "stage" {
+  type        = string
+  description = "stage step of project (dev, staging, prod)"
+}
+
 variable "aws_region" {
   type        = string
   default     = "ap-northeast-2"
