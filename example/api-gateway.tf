068186dd8f24d6219308c9b6877f9427021c3761
kwangbok <kblee@healthport.co.kr>
2024-03-25 01:52:10 +0900

    feat: api-gateway

--- example/api-gateway.tf
+++ example/api-gateway.tf
@@ -13,3 +13,15 @@ resource "aws_apigatewayv2_route" "default" {
   api_id    = aws_apigatewayv2_api.this.id
   route_key = "$default"
 }
+
+resource "aws_apigatewayv2_authorizer" "this" {
+  api_id                            = aws_apigatewayv2_api.this.id
+  authorizer_type                   = "JWT"
+  jwt_configuration {
+    audience = ["${aws_cognito_user_pool_client.client.id}"]
+    issuer   = "https://cognito-idp.${var.aws_region}.amazonaws.com/${aws_cognito_user_pool.pool.id}"
+  }
+  identity_sources                  = ["$request.header.Authorization"]
+  name                              = "${var.project_name}-${var.stage}-authorizer"
+  authorizer_payload_format_version = "2.0"
+}
