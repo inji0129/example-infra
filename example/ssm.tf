909565cff97b6db163add22350c4394cc493dce8
광복 <kblee@artue.io>
2024-03-25 12:06:50 +0900

    base settings

--- example/ssm.tf
+++ example/ssm.tf
@@ -12,3 +12,23 @@ resource "aws_ssm_parameter" "api_gateway_authorizer_id" {
   value = aws_apigatewayv2_authorizer.this.id
 }
 
+resource "aws_ssm_parameter" "cognito_user_pool_client_id" {
+  name  = "${var.project_name}-${var.stage}-cognito-user-pool-client-id"
+  description = "Cognito User Pool Client ID for ${var.project_name} ${var.stage}"
+  type  = "SecureString"
+  value = aws_cognito_user_pool_client.client.id
+}
+
+resource "aws_ssm_parameter" "cognito_user_pool_client_secret" {
+  name  = "${var.project_name}-${var.stage}-cognito-user-pool-client-secret"
+  description = "Cognito User Pool Client Secret for ${var.project_name} ${var.stage}"
+  type  = "SecureString"
+  value = aws_cognito_user_pool_client.client.client_secret
+}
+
+resource "aws_ssm_parameter" "cognito_domain_url" {
+  name  = "${var.project_name}-${var.stage}-cognito-domain-url"
+  description = "Cognito Domain URL for ${var.project_name} ${var.stage}"
+  type  = "SecureString"
+  value = aws_cognito_user_pool_domain.main.domain
+}
