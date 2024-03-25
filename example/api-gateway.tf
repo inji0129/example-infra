95feece8419ba56bccebd636581a23744249abcd
광복 <kblee@artue.io>
2024-03-25 10:22:23 +0900

    base settings

--- example/api-gateway.tf
+++ example/api-gateway.tf
@@ -3,11 +3,13 @@ resource "aws_apigatewayv2_api" "this" {
   protocol_type = "HTTP"
 }
 
-resource "aws_apigatewayv2_integration" "this" {
-  api_id           = aws_apigatewayv2_api.this.id
-  integration_type = "HTTP_PROXY"
-  payload_format_version = "2.0"
-}
+#resource "aws_apigatewayv2_integration" "this" {
+#  api_id           = aws_apigatewayv2_api.this.id
+#  integration_type = "HTTP_PROXY"
+#  payload_format_version = "2.0"
+#
+#  integration_uri = aws_lambda_function.example.invoke_arn
+#}
 
 resource "aws_apigatewayv2_route" "default" {
   api_id    = aws_apigatewayv2_api.this.id
